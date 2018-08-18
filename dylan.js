1fc7da123f88ca359702cf3a8b96c4ec0cc52a83
lstigale <leahstigale@gmail.com>
2018-08-17 21:45:21 -0700

    I'm uploading the basic layout for a friends project

---
+++ dylan.js
@@ -0,0 +1,5 @@
+function nextLine() {
+  document.getElementById("story").innerHTML = "And I'd like to take a minute, just sit right there";
+  document.getElementById('picture').src = "fresh2.jpg";
+  document.getElementById("chapter").innerHTML = "1";
+}
