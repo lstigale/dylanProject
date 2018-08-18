6fd993d3a784e2a3003d4e342e042fefc310459e
lstigale <leahstigale@gmail.com>
2018-08-18 09:15:58 -0700

    Added all the files and made it loop, still need to style.

--- dylan.js
+++ dylan.js
@@ -1,5 +1,71 @@
-function nextLine() {
-  document.getElementById("story").innerHTML = "And I'd like to take a minute, just sit right there";
+function chapter1() {
+  document.getElementById("story").innerHTML = "And I'd like to take a minute, just sit right there<br>I'll tell you how I became the prince of a town called <span onclick='chapter2()' class='underline'>Bel-Air</span>";
   document.getElementById('picture').src = "fresh2.jpg";
   document.getElementById("chapter").innerHTML = "1";
+}
+
+function chapter2() {
+  document.getElementById("story").innerHTML = "In West Philadelphia born and raised<br>On the playground is where I spent most of my <span onclick='chapter3()' class='underline'>days</span>";
+  document.getElementById('picture').src = "fresh3.jpg";
+  document.getElementById("chapter").innerHTML = "2";
+}
+
+function chapter3() {
+  document.getElementById("story").innerHTML = "Chilling out, maxing, relaxing all cool<br>And all shooting some b-ball outside of the <span onclick='chapter4()' class='underline'>school</span>";
+  document.getElementById('picture').src = "fresh4.jpg";
+  document.getElementById("chapter").innerHTML = "3";
+}
+
+function chapter4() {
+  document.getElementById("story").innerHTML = "When a couple of guys who were up to no good<br>Started making trouble in my <span onclick='chapter5()' class='underline'>neighborhood</span>";
+  document.getElementById('picture').src = "fresh5.jpg";
+  document.getElementById("chapter").innerHTML = "4";
+}
+
+function chapter5() {
+  document.getElementById("story").innerHTML = "I got in one little fight and my mom got scared<br>And said \"You're moving with your auntie and uncle in <span onclick='chapter6()' class='underline'>Bel-Air</span>\"";
+  document.getElementById('picture').src = "fresh6.jpg";
+  document.getElementById("chapter").innerHTML = "5";
+}
+
+function chapter6() {
+  document.getElementById("story").innerHTML = "I begged and pleaded with her day after day<br>But she packed my suitcase and sent me on my way<br>She gave me a kiss and then she gave me my ticket<br>I put my Walkman on and said \"I might as well kick <span onclick='chapter7()' class='underline'>it</span>'\"";
+  document.getElementById('picture').src = "fresh7.jpg";
+  document.getElementById("chapter").innerHTML = "6";
+}
+
+function chapter7() {
+  document.getElementById("story").innerHTML = "First class, yo, this is bad<br>Drinking orange juice out of a champagne glass<br>Is this what the people of Bel-Air living like?<br>Hmmm, this might be all <span onclick='chapter8()' class='underline'>right</span>'";
+  document.getElementById('picture').src = "fresh8.jpg";
+  document.getElementById("chapter").innerHTML = "7";
+}
+
+function chapter8() {
+  document.getElementById("story").innerHTML = "But wait, I hear they're prissy, bourgeois, and all that<br>Is this the type of place that they should sent this cool cat?<br>I don't think so, I'll see when I get there<br>I hope they're prepared for the Prince of <span onclick='chapter9()' class='underline'>Bel-Air</span>'";
+  document.getElementById('picture').src = "fresh9.jpg";
+  document.getElementById("chapter").innerHTML = "8";
+}
+
+function chapter9() {
+  document.getElementById("story").innerHTML = "Well, uh, the plane landed and when I came out<br>There was a dude looked like a cop standing there with my name out<br>I ain't trying to get arrested yet, I just got here<br>I sprang with the quickness like lightning, <span onclick='chapter10()' class='underline'>disappeared</span>'";
+  document.getElementById('picture').src = "fresh10.jpg";
+  document.getElementById("chapter").innerHTML = "9";
+}
+
+function chapter10() {
+  document.getElementById("story").innerHTML = "I whistled for a cab and when it came near<br>The license plate said 'Fresh' and it had dice in the mirror<br>If anything I could say that this cab was rare<br>But I thought \"Nah, forget it, yo holmes, to <span onclick='chapter11()' class='underline'>Bel-Air!</span>'\"";
+  document.getElementById('picture').src = "fresh11.jpg";
+  document.getElementById("chapter").innerHTML = "10";
+}
+
+function chapter11() {
+  document.getElementById("story").innerHTML = "I pulled up to a house about seven or eight<br>And I yelled to the cabbie \"Yo holmes, smell ya later\"<br>Looked at my kingdom, I was finally there<br>To sit on my throne as the Prince of <span onclick='chapter0()' class='underline'>Bel-Air</span>'";
+  document.getElementById('picture').src = "fresh12.jpg";
+  document.getElementById("chapter").innerHTML = "11";
+}
+
+function chapter11() {
+  document.getElementById("story").innerHTML = "Now this is a story all about how<br>My life got flipped turned upside <span onclick='chapter1()' class='underline'>down</span>";
+  document.getElementById('picture').src = "fresh1.jpg";
+  document.getElementById("chapter").innerHTML = "0";
 }
